479707610d1884c690a98e16575ec17a96ece25a
Anik Sen <75422517+senanik360@users.noreply.github.com>
2022-07-13 17:09:57 +0600

    Add files via upload

--- vector_1.cpp
+++ vector_1.cpp
@@ -54,49 +54,13 @@ int main()
     v3 = {34,54,23};
     cout<<v3.empty()<<endl;//its a boolean function, so return 0/1
 
-     v3.resize(10); //now vector size is 10 and rest of the elements is 0
+    v3.resize(10); //now vector size is 10 and rest of the elements is 0
 
-      for(int i=0; i<v3.size(); i++)
+    for(int i=0; i<v3.size(); i++)
     {
         cout<<v3[i]<< " ";
     }
-    /*   vector<int> v;
-       v.push_back(2);
-       v.push_back(3);
-       v.push_back(5);
-       v.push_back(7);
+    vector <int> v4(10); //works exactly like array
+    vector <int> v5(10,3); //assign 3 to all the indexes
 
-       v[3]=9;
-
-       for(int i=0; i<v.size(); i++)
-       {
-           cout<<v[i]<<" ";
-       }
-       cout<<endl;
-       vector<int> v1;
-       int n,a;
-       cin>>n;
-       for(int i=0; i<n; i++)
-       {
-           cin>>a;
-           v1.push_back(a);
-       }
-       for(int i=0; i<v1.size(); i++)
-       {
-           cout<<v1[i]<<" ";
-       }
-       cout<<endl;
-       vector <int> v2 = {101,102,103,104};
-       for(int i=0; i<v2.size(); i++)
-       {
-           cout<<v2[i]<<" ";
-       }
-       cout<<endl;
-       v2.clear(); // vector elements clear
-       cout<<v2.empty()<<endl; //boolean function, print 0/1 whether its empty or not
-       v2.resize(10); // vector resize kore dibe & zero print korbe
-
-       vector <int> v3(10); //works exactly like array
-       vector <int> v4(10,3); //shob gula index a 3 assign kore dibe
-    */
 }
